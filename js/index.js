9c4a7458ed4f0f1fcf4046e1f234d3d3b411dbc8
thakursachin467 <thakursachin467@gmail.com>
2018-03-27 00:20:49 +0530

    added

---
+++ js/index.js
@@ -0,0 +1,17 @@
+window.sr=ScrollReveal();
+/*sr.reveal('.main',{
+  duration:2000,
+  opacity: 0,
+  origin:'left',
+  distance:'300px',
+
+
+}); */
+
+sr.reveal('.ptext',{
+  duration:2000,
+  origin:'left',
+  easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+  distance:'400px',
+  rotate: { x:0, y: 40, z: 0 }
+});
